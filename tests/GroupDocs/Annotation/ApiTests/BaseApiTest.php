e9bbf673c61a43a88840264575fee29894a46b46
vitaliy-rezchikov <vitaliy.rezchikov@groupdocs.com>
2018-05-22 16:57:19 +0300

    Updated year on copyright headers

--- tests/GroupDocs/Annotation/ApiTests/BaseApiTest.php
+++ tests/GroupDocs/Annotation/ApiTests/BaseApiTest.php
@@ -2,7 +2,7 @@
 /*
 * --------------------------------------------------------------------------------------------------------------------
 * <copyright company="Aspose" file="BaseApiTest.php">
-*   Copyright (c) 2017 Aspose.Words for Cloud
+*   Copyright (c) 2003-2018 Aspose Pty Ltd
 * </copyright>
 * <summary>
 *   Permission is hereby granted, free of charge, to any person obtaining a copy
